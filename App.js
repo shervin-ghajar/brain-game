0f1efb2b95c741b4a7b113e9d16698c5f63b61f8
Shervin <shervin@Shervins-MacBook-Pro.local>
2020-02-21 13:47:27 +0330

    intro ui improved/package install

--- App.js
+++ App.js
@@ -2,48 +2,69 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import LottieView from 'lottie-react-native';
 import Intro from './assets/animations/intro.json'
-
+//--------------------------------------------------------------------------------------------
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      showTitle: false,
     };
   }
-  renderGameTitle() {
-    return (
-      <View>
-        <Text style={styles.titleStyle}>BRAIN GAMES</Text>
-      </View>
-    )
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.handleTitle()
+    }, 1300);
   }
+  handleTitle = () => {
+    this.setState({ showTitle: true })
+  }
+  renderIntroTitle() {
+    if (this.state.showTitle) {
+      return (
+        <View>
+          <Text style={styles.titleStyle}>BRAIN GAMES</Text>
+        </View>
+      )
+    }
+  }
+
   render() {
     return (
       <View style={styles.view}>
         <View style={styles.animationContainer}>
-          <LottieView source={Intro} autoPlay loop resizeMode="cover" />
+          <LottieView
+            source={Intro}
+            // onAnimationFinish={() => this.handleTitle()}
+            autoPlay
+            loop={false}
+            resizeMode="cover" />
         </View>
-        {this.renderGameTitle()}
+        {this.renderIntroTitle()}
       </View>
     );
   }
 }
-
+//--------------------------------------------------------------------------------------------
 const styles = {
   view: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: "#2c2861"
+    backgroundColor: "#FFF6AF"
   },
   animationContainer: {
-    width: 200,
-    height: 200,
+    width: 250,
+    height: 250,
     alignItems: "center",
     marginBottom: 50,
   },
   titleStyle: {
-    color: "yellow",
+    color: "#444A54",
     fontSize: 25,
-    fontFamily: "#F2DA69"
+    fontFamily: "serif",
+    fontWeight: "bold",
+    position: "absolute",
+    alignSelf: "center",
   }
 }
